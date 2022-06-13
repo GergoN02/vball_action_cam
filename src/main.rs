b98922898b676b248ad4607079e7f96ab9613389
GergoN02 <nemethgergo02@gmail.com>
2022-06-13 18:36:37 +0100

    cam test

--- src/main.rs
+++ src/main.rs
@@ -3,25 +3,25 @@ use nokhwa::{Camera, CameraFormat, CameraInfo, CaptureAPIBackend, NokhwaError};
 fn main() {
     println!("Hello, world!");
 
-    let dev = nokhwa::query_devices(CaptureAPIBackend::Auto);
+    // let dev = nokhwa::query_devices(CaptureAPIBackend::Auto);
 
-    println!("{:?}", dev);
+    // println!("{:?}", dev);
 
-    // let mut camera = Camera::new(
-    //     0,
-    //     Some(CameraFormat::new_from(
-    //         640,
-    //         480,
-    //         nokhwa::FrameFormat::MJPEG,
-    //         30,
-    //     )),
-    // )
-    // .unwrap();
+    let mut camera = Camera::new(
+        0,
+        Some(CameraFormat::new_from(
+            640,
+            480,
+            nokhwa::FrameFormat::MJPEG,
+            30,
+        )),
+    )
+    .unwrap();
 
-    // camera.open_stream().unwrap();
+    camera.open_stream().unwrap();
 
-    // loop {
-    //     let frame = camera.frame().unwrap();
-    //     println!("{}, {}", frame.width(), frame.height());
-    // }
+    loop {
+        let frame = camera.frame().unwrap();
+        println!("{}, {}", frame.width(), frame.height());
+    }
 }
